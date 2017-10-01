0a64b486cf688b1fb00dae3367aba175d052fdcb
Misha Zapalenov <mzapalenov@mail.ru>
2017-10-01 13:26:34 +0300

    Add system change privilege

--- mvc/Controller/Profile/ProfileFunction/Privilege/ChangePrivilege.php
+++ mvc/Controller/Profile/ProfileFunction/Privilege/ChangePrivilege.php
@@ -3,7 +3,29 @@
 namespace liw\mvc\Controller\Profile\ProfileFunction\Privilege;
 
 
+use liw\mvc\Model\Profile\ProfileUser;
+use liw\mvc\Model\Profile\Users;
+
 class ChangePrivilege
 {
+    public function checkPrivilege($nickname, $privilege, $currentPrivilege)
+    {
+        if ($currentPrivilege != 'admin') {
+            die("Вы не админ!");
+        }
 
+        $this->setPrivilege($nickname, $privilege);
+    }
+
+    private function setPrivilege($nickname, $privilege)
+    {
+        $users       = new Users();
+        $profileUser = new ProfileUser();
+
+        $users->connectBD();
+        $profileUser->connectBD();
+
+        $users->updatePrivilege($nickname,$privilege);
+        $profileUser->updatePrivilege($nickname,$privilege);
+    }
 }
